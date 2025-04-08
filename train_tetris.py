5b41e38b09514805b10fa79c50980f826a60c89d
Spucky <126801350+Spucky1@users.noreply.github.com>
2025-04-07 21:07:42 -0500

    TETRI
    tETRIS

--- train_tetris.py
+++ train_tetris.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 import ale_py
+import sys
 import stable_baselines3
 from stable_baselines3 import A2C
 
@@ -8,17 +9,7 @@ gym.register_envs(ale_py)
 env = gym.make("ALE/Tetris-v5", render_mode = "human")
 
 model = A2C("MlpPolicy", env, verbose =1)
-model.learn(total_timesteps= 1000)
+model.learn(total_timesteps=10000 , progress_bar=True)
+model.save("a2c.tetris")
 
-
-episodes = 10
-
-for ep in range(episodes):
-   obs = env.reset()
-   done = False
-   while not done:
-      env.render()
-      env.step(env.action_space.sample())
-
-env.close()
 
