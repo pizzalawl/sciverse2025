0058f314e93f546124df146683e8cc9da6362964
Spucky <126801350+Spucky1@users.noreply.github.com>
2025-04-07 21:31:38 -0500

    Update train_tetris.py

--- train_tetris.py
+++ train_tetris.py
@@ -1,15 +1,25 @@
 import gymnasium as gym
 import ale_py
 import sys
+<<<<<<< HEAD
 import stable_baselines3
 from stable_baselines3 import PPO
+=======
+from stable_baselines3 import A2C
+>>>>>>> 4769bb8360c801d744b776c6d1ec430768e8cbfc
 
 gym.register_envs(ale_py)
 
 env = gym.make("ALE/Tetris-v5", render_mode = "human")
 
+<<<<<<< HEAD
 model = PPO("MlpPolicy", env, verbose =1)
 model.learn(total_timesteps=10000 , progress_bar=True)
 model.save("a2c.tetris")
+=======
+model = A2C("MlpPolicy", env, verbose =1)
+model.learn(total_timesteps=sys.argv[1] , progress_bar=True)
+model.save("a2c_tetris.zip")
+>>>>>>> 4769bb8360c801d744b776c6d1ec430768e8cbfc
 
 
