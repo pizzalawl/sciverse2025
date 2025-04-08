4769bb8360c801d744b776c6d1ec430768e8cbfc
pizzalawl <97758804+pizzalawl@users.noreply.github.com>
2025-04-07 21:15:12 -0500

    fix namings on tetris

--- train_tetris.py
+++ train_tetris.py
@@ -1,7 +1,6 @@
 import gymnasium as gym
 import ale_py
 import sys
-import stable_baselines3
 from stable_baselines3 import A2C
 
 gym.register_envs(ale_py)
@@ -9,7 +8,7 @@ gym.register_envs(ale_py)
 env = gym.make("ALE/Tetris-v5", render_mode = "human")
 
 model = A2C("MlpPolicy", env, verbose =1)
-model.learn(total_timesteps=10000 , progress_bar=True)
-model.save("a2c.tetris")
+model.learn(total_timesteps=sys.argv[1] , progress_bar=True)
+model.save("a2c_tetris.zip")
 
 
