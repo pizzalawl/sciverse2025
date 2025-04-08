901302e992f34d387ae7efdcac82160d02d968c5
pizzalawl <97758804+pizzalawl@users.noreply.github.com>
2025-04-07 21:38:08 -0500

    new folder structure and switch shit to PPO

---
+++ train_mario.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+import ale_py
+import sys
+from stable_baselines3 import PPO
+
+gym.register_envs(ale_py)
+
+env = gym.make("ALE/MarioBros-v5", render_mode ="human")
+
+model = PPO("MlpPolicy", env, verbose =1, tensorboard_log="./logs/mario/")
+model.learn(total_timesteps=int(sys.argv[1]) , progress_bar=True)
+model.save("pretrained/mario/ppo_mario.zip")
--- train_tetris.py
+++ train_tetris.py
@@ -1,14 +1,12 @@
 import gymnasium as gym
 import ale_py
 import sys
-from stable_baselines3 import A2C
+from stable_baselines3 import PPO
 
 gym.register_envs(ale_py)
 
-env = gym.make("ALE/Tetris-v5", render_mode = "human")
+env = gym.make("ALE/Tetris-v5", render_mode="human")
 
-model = A2C("MlpPolicy", env, verbose =1)
-model.learn(total_timesteps=sys.argv[1] , progress_bar=True)
-model.save("a2c_tetris.zip")
-
-
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/tetris/")
+model.learn(total_timesteps=int(sys.argv[1]) , progress_bar=True)
+model.save("pretrained/tetris/ppo_tetris.zip")
